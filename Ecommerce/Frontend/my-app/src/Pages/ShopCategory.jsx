a951f42b93ea4f3a1c34156db6a2d75968d0fa48
Emmanuel <sarpbe808@gmail.com>
2025-04-06 16:51:36 +0000

    ...

--- Ecommerce/Frontend/my-app/src/Pages/ShopCategory.jsx
+++ Ecommerce/Frontend/my-app/src/Pages/ShopCategory.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
 
-function ShopCategory() {
-  return <div></div>;
+function ShopCategory(props) {
+  // Destructuring
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img src={props.banner} alt="" />
+    </div>
+  );
 }
 
 export default ShopCategory;
